026f67f01a3213b873089998505267764ad0f5d9
fayshal69 <foyshalbinamir@gmail.com>
2024-02-28 01:37:51 +0600

    finished the project

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const loadingBar = document.getElementById('loadingBar');
 
 
 // load all phones by api
-const loadPhones = async (searchText = '13') => {
+const loadPhones = async (searchText = '13', isSeeAllPhone) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones);
+    displayPhones(phones, isSeeAllPhone);
 }
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isSeeAllPhone) => {
     // clear the card container div before inset cards
     phoneCardContainer.innerHTML = '';
 
     // if no data found
-    if(phones.length === 0) {
+    if (phones.length === 0) {
         noDataFoundMsg.classList.remove('hidden');
         handleLoadingBar(false);
     }
@@ -26,8 +26,8 @@ const displayPhones = (phones) => {
         noDataFoundMsg.classList.add('hidden');
     }
 
-    // only 9 card will be displayed
-    if(phones.length > 9) {
+    // only 9 card will be displayed and if see all btn is not clicked
+    if (phones.length > 9 && !isSeeAllPhone) {
         phones = phones.slice(0, 9);
         seeMoreBtn.classList.remove('hidden');
     }
@@ -47,7 +47,7 @@ const displayPhones = (phones) => {
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>There are many variations of passages of available, but the majority have suffered</p>
                 <div class="card-actions justify-center">
-                    <button class="btn btn-primary">See Details</button>
+                    <button class="btn btn-primary" onclick="seeDetailsHandler('${phone.slug}')">See Details</button>
                 </div>
             </div>
         `;
@@ -56,25 +56,58 @@ const displayPhones = (phones) => {
     }
 }
 
-const searchPhones = () => {
+const searchPhones = (isSeeAllPhone) => {
     const searchBoxText = searchBox.value;
-    if(searchBoxText.trim() === '') {
+    if (searchBoxText.trim() === '') {
         loadPhones();
         return;
     }
     else {
         handleLoadingBar(true);
-        loadPhones(searchBoxText);
+        loadPhones(searchBoxText, isSeeAllPhone);
     }
 }
 
 const handleLoadingBar = (isLoading) => {
-    if(isLoading) {
+    if (isLoading) {
         loadingBar.classList.remove('hidden');
     }
     else {
         loadingBar.classList.add('hidden');
     }
+}
+
+const seeAllPhones = () => {
+    searchPhones(true);
+}
+
+const seeDetailsHandler = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const details = data.data;
+    showPhoneDetails(details);
+}
+
+const showPhoneDetails = (details) => {
+    const showDetailContainer = document.getElementById('show-detail-container');
+    showDetailContainer.innerHTML = `
+        <div class="text-center"><img src="${details.image}" class="inline"></div>
+        <div class="space-y-3">
+            <h3 class="text-xl font-semibold">${details?.name || 'Not defined'}</h3>
+            <div class="space-y-2 opacity-80">
+                <p><span class="font-semibold">Storage: </span> ${details?.mainFeatures?.storage || 'Not defined'}</p>
+                <p><span class="font-semibold">Display Size: </span> ${details?.mainFeatures?.displaySize || 'Not defined'}</p>
+                <p><span class="font-semibold">Chipset: </span> ${details?.mainFeatures?.chipSet || 'Not defined'}</p>
+                <p><span class="font-semibold">Memory: </span> ${details?.mainFeatures?.memory || 'Not defined'}</p>
+                <p><span class="font-semibold">Slug: </span> ${details?.slug || 'Not defined'}</p>
+                <p><span class="font-semibold">Release data: </span> ${details?.releaseDate || 'Not defined'}</p>
+                <p><span class="font-semibold">Brand: </span> ${details?.brand || 'Not defined'}</p>
+                <p><span class="font-semibold">GPS: </span> ${details?.other?.GPS || 'Not defined'}</p>
+            </div>
+        </div>
+    `;
+    // display the modal
+    my_modal_5.showModal();
 }
 
 loadPhones();
